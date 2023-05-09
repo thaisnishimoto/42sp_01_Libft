b49cedd6aadd7e3448ac4b02b5ebb4ae8a2b3656
Thais Nishimoto <thaismnishimoto@gmail.com>
2023-05-09 00:37:32 -0300

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,15 +6,17 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:20:54 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/05/08 18:47:19 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/05/09 00:34:32 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int	ft_atoi(const char *nptr)
 {
-	unsigned char	i;
+	int	i;
 	int				sign;
 	int				num;
 
@@ -22,29 +24,29 @@ int	ft_atoi(const char *nptr)
 	while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\n'
 		|| nptr[i] == '\v' || nptr[i] == '\f' || nptr[i] == '\r')
 		i++;
-	sign = 0;
-	if (nptr[i++] == '-')
-	{
+	sign = 1;
+	if (nptr[i] == '-')
 		sign = -1;
-		while (nptr[i] == '-')
-			i++;
-		if (nptr[i] == '+')
-			return (0);
-	}
-	else if (nptr[i] == '+')
-	{
-		while (nptr[i] == '+')
-			i++;
-		if (nptr[i] == '-')
-			return (0);
-	}
+	if (nptr[i] == '-' || nptr[i] == '+')
+		i++;
 	num = 0;
-	if (nptr[i] < '0' && nptr[i] > '9')
-		return (0);
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		num = num * 10 + (nptr[i] - '0');
 		i++;
 	}
 	return (sign * num);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	mine;
+	int	theirs;
+	if (argc == 2)
+	{
+		mine = ft_atoi(argv[1]);
+		theirs = atoi(argv[1]);
+		printf("mine = %d | theirs = %d\n", mine, theirs);
+	}
+	return (0);
 }

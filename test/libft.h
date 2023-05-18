4db82cbd658ad6b9af8e93fec0dc88e304904808
thaisnishimoto <126536908+thaisnishimoto@users.noreply.github.com>
2023-05-18 19:58:28 -0300

    Update libft.h

--- test/libft.h
+++ test/libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brunrodr <brunrodr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 19:57:20 by brunrodr          #+#    #+#             */
+/*   Updated: 2023/05/18 19:57:21 by brunrodr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -67,7 +79,5 @@ void				ft_putstr_fd(char *s, int fd);
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
-
-
 
 #endif

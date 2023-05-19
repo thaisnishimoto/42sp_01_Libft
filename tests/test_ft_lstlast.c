bd0c62e4a55590007da03d9db813fd5ee7f8d6a1
Thais Mina nishimoto <tmina-ni@c1r7p2.42sp.org.br>
2023-05-19 18:09:49 -0300

    lstlast test

---
+++ tests/test_ft_lstlast.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_lstlast.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 12:00:56 by tmina-ni          #+#    #+#             */
+/*   Updated: 2023/05/19 17:55:49 by tmina-ni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include "minunit.h"
+#include <stdlib.h>
+
+MU_TEST(should_return_content_of_last_node)
+{
+	t_list    *lst = NULL;
+	t_list    *upd;
+
+	ft_lstadd_front(&lst, ft_lstnew("1")); //fourth node
+	ft_lstadd_front(&lst, ft_lstnew("2")); //third node
+	ft_lstadd_front(&lst, ft_lstnew("3")); //second node
+	ft_lstadd_front(&lst, ft_lstnew("4")); //first node
+	upd = lst;
+	while (upd)
+	{
+		printf("%s\n", (char *)upd->content);
+		upd = upd->next;
+	}
+	char	expected[] = "1";
+	upd = ft_lstlast(lst);
+	mu_assert_string_eq(expected, (char *)upd->content);
+}
+
+MU_TEST_SUITE(test_suite)
+{
+	MU_RUN_TEST(should_return_content_of_last_node);
+}
+
+int	main(void)
+{
+	MU_RUN_SUITE(test_suite);
+	MU_REPORT();
+	return MU_EXIT_CODE;
+}

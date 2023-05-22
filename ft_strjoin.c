b933b0422f4d27d3098eab1c3531cf5b659b592b
Thais Mina nishimoto <tmina-ni@c1r10p4.42sp.org.br>
2023-05-22 11:33:31 -0300

    strjoin corrected

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 23:39:08 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/05/11 16:48:05 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/05/22 11:32:23 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,17 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
-	size_t	i;
 	char	*ptr;
-	char	*ptr2;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	ptr = malloc((s1_len + s2_len + 1) * 1);
 	if (ptr == NULL)
 		return (NULL);
-	i = 0;
-	ptr = ft_memmove(ptr, s1, s1_len);
-	ptr2 = ft_memmove(&ptr[s1_len], s2, s2_len);
+	ft_memmove(ptr, s1, s1_len);
+	ft_memmove(&ptr[s1_len], s2, s2_len);
 	ptr[s1_len + s2_len] = '\0';
 	return (ptr);
 }

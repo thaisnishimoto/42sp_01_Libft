c44cc7a70802ffdbaf2dbf641b420302936dd8ed
Thais Mina nishimoto <tmina-ni@c2r8p6.42sp.org.br>
2023-10-18 15:17:40 -0300

    added test for +0 and -0

--- tests/test_ft_atoi.c
+++ tests/test_ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 12:00:56 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/05/09 11:05:03 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/10/18 15:17:01 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "minunit.h"
 #include <stdlib.h>
 
-MU_TEST(compate_to_original_atoi)
+MU_TEST(compare_to_original_atoi)
 {
 	int	result = ft_atoi("\n +42");
 	int	expected = atoi("\n +42");
@@ -47,11 +47,31 @@ MU_TEST(should_return_minus_1_to_spaces_before)
 	mu_assert_int_eq(expected, result);
 }
 
+MU_TEST(should_return_0_to_signed_0)
+{
+	int	result = ft_atoi(" +0");
+	int	expected = atoi(" +0");
+	
+	printf("\\n +0: mine = %d | original = %d\n", result, expected);
+	mu_assert_int_eq(expected, result);
+}
+
+MU_TEST(should_return_0_to_minus_0)
+{
+	int	result = ft_atoi(" --0");
+	int	expected = atoi(" --0");
+	
+	printf(" --0: mine = %d | original = %d\n", result, expected);
+	mu_assert_int_eq(expected, result);
+}
+
 MU_TEST_SUITE(test_suite)
 {
-	MU_RUN_TEST(compate_to_original_atoi);
+	MU_RUN_TEST(compare_to_original_atoi);
 	MU_RUN_TEST(should_return_0_to_multiple_signs);
 	MU_RUN_TEST(should_return_minus_1_to_spaces_before);
+	MU_RUN_TEST(should_return_0_to_signed_0);
+	MU_RUN_TEST(should_return_0_to_minus_0);
 }
 
 int	main(void)
